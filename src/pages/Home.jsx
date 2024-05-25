0b78d50ad047e82ae782c6adcd57494612dd9c6c
coolhappy2002 <coolhappy2002@gmail.com>
2024-05-25 22:39:13 +0530

    Home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         <Link to={"/signup"}>
             <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit'>
                 <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900'>
-                    <p>Become an Instructor</p>
+                    <p>Become an Instructors</p>
                     <FaArrowRight />
                 </div>
             </div>
