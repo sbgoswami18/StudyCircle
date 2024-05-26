8958da36081f09873ab7060f4dd3cea462eefc71
coolhappy2002 <coolhappy2002@gmail.com>
2024-05-26 15:41:24 +0530

    Changes in About's Reviewslider

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -110,7 +110,7 @@ const About = () => {
             <ContactFormSection />
         </section>
 
-        <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+        <div className="relative mx-auto my-20 w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
             {/* Reviws from Other Learner */}
             <h1 className="text-center text-4xl font-semibold mt-8">
                 Reviews from other learners
